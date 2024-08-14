2a20706e622ccab38348170f3021614a2981e5d7
Gabriel <gabriel.slmendoza@gmail.com>
2024-08-13 22:46:17 -0500

    add: profile img

--- src/components/Auth/User.tsx
+++ src/components/Auth/User.tsx
@@ -3,8 +3,9 @@ import { IUser } from '../../types/User'
 
 export const User = ({ user, children:_ }: { user: IUser, children?: ReactNode }) => {
 	return (
-		<div className='d-flex align-items-center'>
+		<div className='d-flex align-items-center gap-1'>
 			<p className='m-0'><span>Welcome,</span> <strong>{ user.name } {user.lastName}</strong></p>
+			<img src="/img/john-doe.jpg" className='rounded-circle' alt="profile" width={28} height={28} />
 		</div>
 	)
 }
